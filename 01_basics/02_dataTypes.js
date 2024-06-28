5fd37697712e0b3e22289e560d63882baee1769e
Anjali Singh <102979716+anjali-482@users.noreply.github.com>
2024-06-28 10:00:48 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Anjali")
+
+let name = 'anjali'
+let age = 20
+let isLoggesIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
