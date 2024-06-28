eeb33c304320e92526f212763d55b84bae208800
Anjali Singh <102979716+anjali-482@users.noreply.github.com>
2024-06-28 10:23:06 +0000

    Conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+// let score = undefined
+
+// console.log(typeof score); //string
+// console.log(typeof(score));
+
+// let valueInNumber = Number
+
+// let valueInNumber = Number(score) 
+// console.log(valueInNumber); //NaN
+// console.log(typeof valueInNumber); //NUMBER
+
+
+// "33" => easily converts to 33
+// "33abc" => NaN
+// true =>1 ; false =>0
+
+let isLoggedIn = "anjali"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+//1 => true; 0 => false
+// "" => false
+//"anjali" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
