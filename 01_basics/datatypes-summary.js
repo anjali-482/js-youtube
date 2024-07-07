7159eb92296c0a9f6df8ef5d599c0700db936f8c
Anjali Singh <102979716+anjali-482@users.noreply.github.com>
2024-07-07 11:53:51 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 325465654654654655n
+
+// Reference Type (non-primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "anjali",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+// console.log(typeof bigNumber);
+
+console.log(typeof outsideTemp); //ye to null hai pr o/p me object aaya bcz 
+
+
+console.log(typeof heros)
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
